80f07a991dfa4919b9da55610e0f9b74ef724ab2
dutsn <giihrapozinha13@gmail.com>
2026-05-29 15:32:43 -0300

    thor

--- Assets/Scripts/PersonagemController.cs
+++ Assets/Scripts/PersonagemController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PersonagemController : MonoBehaviour
-{
+{ 
+    public Rigidbody2D rb2d;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
